c2af3be87be34df77ebe59dfd68daca9b9b4c3b3
Amandine4731 <5618@holbertonstudents.com>
2023-06-19 07:38:34 -0700

    delete older version

--- Flutter/my_student_flat_advanced/OLD ANDROID/app/src/main/kotlin/com/example/my_student_flat_4/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.my_student_flat_4
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Flutter/my_student_flat_advanced/android/app/src/main/kotlin/com/example/my_student_flat_advanced/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.my_student_flat_advanced
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
